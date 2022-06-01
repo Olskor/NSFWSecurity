5957f81757c0a6a563d919c6d7b3d5ca8060cabc
jbami <jbami@DESKTOP-NLKEFFI>
2022-06-01 18:32:58 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.SDK3.Dynamics.Contact;
+
+namespace NSFWSecurity
+{
+    public partial class Program : MelonMod
+    {
+
+        private static MelonPreferences_Entry<bool> ourIsEnabled;
+
+        public override void OnApplicationStart()
+        {
+            VRCHooks.OnApplicationStart(HarmonyInstance);
+            VRCHooks.AvatarIsReady += VRCHooks_AvatarIsReady;
+
+            var category = MelonPreferences.CreateCategory("NSFWSecurity", "NSFWSecurity");
+            ourIsEnabled = category.CreateEntry("Enabled", true, "Enable NSFW");
+        }
+
+        private void VRCHooks_AvatarIsReady(object sender, VRCPlayer player)
+        {
+            if (!ourIsEnabled.Value) return;
+            if (player == Util.LocalPlayer)
+            {
+                var avatar = player.GetComponentInChildren<VRCAvatarManager>().gameObject.GetComponentInChildren<VRC.SDK3.Avatars.Components.VRCAvatarDescriptor>();
+                if (!avatar) return;
+                var contactSender = avatar.gameObject.AddComponent<VRC.Dynamics.ContactSender>();
+                contactSender.collisionTags.Add("NSFW");
+                contactSender.radius = 5;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Util.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using VRC;
+
+namespace NSFWSecurity
+{
+  static class Util
+  {
+    public static PlayerManager PlayerManager => PlayerManager.prop_PlayerManager_0;
+
+    public static VRCPlayer LocalPlayer => VRCPlayer.field_Internal_Static_VRCPlayer_0;
+
+    public static GameObject GetAvatar(this Player player) => player.prop_VRCPlayer_0.prop_VRCAvatarManager_0.prop_GameObject_0;
+
+    #if DEBUG
+      public static bool Debug => true;
+    #else
+      public static bool Debug => false;
+    #endif
+
+    public static bool DebugLogging = Debug; 
+    public static void DebugLog(string message, bool force = false) 
+    {
+      if (DebugLogging || force) {
+        MelonLoader.MelonLogger.Msg(System.ConsoleColor.Cyan, "[DEBUG] " + message);
+      }
+    }
+
+    public static bool AlmostEqual(double a, double b) {
+      const double delta = 0.0001;
+      return Math.Abs(a - b) < delta;
+    }
+
+    static bool IsChildOf(GameObject parent, GameObject child)
+    {
+      if (parent == child) {
+        return true;
+      }
+
+      for (int i = 0; i < parent.transform.childCount; i++) {
+        if (IsChildOf(parent.transform.GetChild(i).gameObject, child)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    // Since there is no GetComponentInParent that gets inactive components...
+    // GetComponent functions not including active components and not making it a conscious
+    // choice for the programmer is one of the biggest mistakes and source of bugs Unity ever caused.
+    public static T GetComponentInParent<T>(Component c, bool inactive) 
+    {
+      var components = c.GetComponentsInParent<T>(inactive);
+      return components.Length > 0 ? components[0] : default(T);
+    }
+  }
+}
